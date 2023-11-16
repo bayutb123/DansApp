b22b5bdd2dd08f55b9a2d6192cbfbb323b314e8f
Bayu Tantra Bramandhita <bayutantra28@gmail.com>
2023-11-16 21:21:18 +0700

    Implement login and home UI

--- app/src/main/java/com/bayutb123/dansapp/ui/home/MainActivity.java
+++ app/src/main/java/com/bayutb123/dansapp/ui/home/MainActivity.java
@@ -1,79 +1,55 @@
 package com.bayutb123.dansapp.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.bayutb123.dansapp.R;
-import com.google.android.material.snackbar.Snackbar;
+import com.bayutb123.dansapp.ui.login.LoginActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.bayutb123.dansapp.databinding.ActivityMainBinding;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        boolean login = getIntent().getBooleanExtra("login", false);
+        if (!login) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+        binding.applyFilter.setOnClickListener(view -> {
+            filter();
+        });
+        binding.filterButton.setOnClickListener(view -> {
+            if (binding.filterLayout.getVisibility() == View.GONE) {
+                binding.filterLayout.setVisibility(View.VISIBLE);
+            } else {
+                binding.filterLayout.setVisibility(View.GONE);
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void filter() {
+        binding.filterLayout.setVisibility(View.GONE);
     }
 }
---
+++ app/src/main/java/com/bayutb123/dansapp/ui/login/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.bayutb123.dansapp.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.bayutb123.dansapp.R;
+import com.bayutb123.dansapp.databinding.ActivityLoginBinding;
+import com.bayutb123.dansapp.ui.home.MainActivity;
+
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.loginButton.setOnClickListener(view -> login(
+            Objects.requireNonNull(binding.username.getText()).toString(),
+            Objects.requireNonNull(binding.password.getText()).toString()
+        ));
+    }
+
+    private void login(String username, String password) {
+        if (username.equals("admin") && password.equals("admin")) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("login", true);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this,
+                    "Masukkan 'admin' sebagai username \ndan password", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
